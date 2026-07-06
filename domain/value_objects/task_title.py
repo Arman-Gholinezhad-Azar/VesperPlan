f62500f3a7d23b1a977629bd5492d94d88cea9a6
Arman <cybertechik@protonmail.com>
2026-07-06 18:10:18 +0330

    created self validating value object of task title

---
+++ domain/value_objects/task_title.py
@@ -0,0 +1,35 @@
+import re
+from dataclasses import dataclass
+
+
+_TASK_TITLE_PATTERN = re.compile(r"^[a-zA-Z0-9_.- ]+$")
+
+@dataclass(frozen=True)
+class TaskTitle:
+
+    _value: str
+
+    def __post_init__(self):
+
+        if not isinstance(self._value, str):
+            raise TypeError("Task title should be string!")
+
+        formatted = self._value.strip()
+        length = len(formatted)
+
+        if not formatted:
+            raise ValueError("Task name should not be empty.")
+
+        if length > 15:
+            raise ValueError("Task name should not be more than 15 characters.")
+
+        if not re.search(_TASK_TITLE_PATTERN, formatted):
+            raise ValueError("Task name cannot use special characters like ($%^& etc). allowed special characters are: (_ . -)")
+
+        object.__setattr__(self, "_value", formatted)
+
+    def __str__(self) -> str:
+        return self._value
+
+    def __repr__(self) -> str:
+        return str(self)
