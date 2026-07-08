3003ed27eefa2ca1665e8230511ba881316a500d
Arman <cybertechik@protonmail.com>
2026-07-08 13:16:43 +0330

    finished category vo

--- domain/value_objects/category.py
+++ domain/value_objects/category.py
@@ -1,7 +1,36 @@
-from enum import Enum
+import re
+from dataclasses import dataclass
 
-class TaskStatus(Enum):
+
+_CATEGORY_PATTERN = re.compile(r"^[a-z0-9_\- ]+$")
+
+
+@dataclass(frozen=True)
+class Category:
+
+    _value:str
+
+    def __post_init__(self):
+
+        if not isinstance(self._value, str):
+            raise TypeError("Category should be string")
+
+        formatted = self._value.strip().lower()
+        length = len(formatted)
+
+        if not formatted:
+            raise ValueError("category cannot be empty!")
+
+        if length > 15:
+            raise ValueError("category cannot be more than 15 characters")
+        
+        if not re.search(_CATEGORY_PATTERN, formatted):
+            raise ValueError("invalid category pattern. correct pattern: (work_house, work-house, work house, work ...)")
     
-    DONE = "done"
-    PENDING = "pending"
-    DELETED = "deleted" # recoverable
+        object.__setattr__(self, "_value", formatted)
+
+    def __str__(self) -> str:
+        return self._value
+    
+    def __repr__(self) -> str:
+        return str(self)
