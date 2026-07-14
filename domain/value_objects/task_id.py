4e9da11fc25e139de9ce38d7ac1f7525d1030ee3
Arman <cybertechik@protonmail.com>
2026-07-14 15:35:11 +0330

    created task id vo

---
+++ domain/value_objects/task_id.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from uuid import UUID, uuid4
+
+
+@dataclass(frozen=True, slots=True)
+class TaskId:
+    value: UUID
+
+    def __post_init__(self):
+
+        if self.value.version != 4:
+            raise ValueError("Task Id must be UUIDv4")
+
+    def __str__(self) -> str:
+        return str(self.value)
